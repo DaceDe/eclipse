ccc824938f279bcd1f52b007d22d15f49bdcd918
Dace De vALDENA <dace_bobovica@inbox.lv>
2019-11-23 13:22:38 +0200

    finish

--- lecture15/src/com/accenture/lecture15/Weather.java
+++ lecture15/src/com/accenture/lecture15/Weather.java
@@ -1,5 +1,41 @@
 package com.accenture.lecture15;
 
+import java.util.Arrays;
+
 public class Weather {
+
+	// Task 1 - Lets make our Weather class to store following information:
+	// String country - for which forecast is prepared.
+	// int[] daysWeather- temperature information for next week (7 days).
+
+	private final String country;
+	private int[] daysWeather;
+
+	public String getCountry() {
+		return country;
+	}
+
+	public int[] getDaysWeather() {
+		return daysWeather;
+	}
+
+	public void setDaysWeather(int[] daysWeather) {
+		this.daysWeather = daysWeather;
+	}
+
+	public Weather(String country, int[] daysWeather) {
+		this.country = country;
+		this.daysWeather = daysWeather;
+	}
+
+	void display() {
+		System.out.println(country + " " + daysWeather);
+
+	}
+
+	public String toString() {
+		return "Weather forecast in country=" + country + ", daysWeather="
+				+ Arrays.toString(daysWeather) + "]";
+	}
 
 }
